5a6b05b178758718b12df3107e1c1979978680ce
Vasili Vasilyeu <recon@graybox.local>
2014-10-22 21:04:46 -0700

    Created dummy distribution package

--- README.md
+++
@@ -1,4 +0,0 @@
-distcovery
-==========
-
-Command test with coverage check and report for distutils setup
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name='Distcovery',
+      version='0.0.1',
+      description='Command "test" for distutils setup',
+      long_description='The library provides command "test" for distutils ' \
+                       'setup. The command allows to run tests ' \
+                       'all together or individually and calculate ' \
+                       'test coverage.',
+      author='Vasili Vasilyeu',
+      author_email='vasili.v@tut.by',
+      url='https://github.com/vasili-v/distcovery',
+      py_modules=['distcovery'],
+      license='MIT',
+      platforms=('Linux', 'Darwin'))
+
