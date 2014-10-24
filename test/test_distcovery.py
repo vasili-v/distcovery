72132b9eafb48513649a9d7d5927e50920559f3a
Vasili Vasilyeu <vasili.v@tut.by>
2014-10-23 20:47:55 -0700

    Added class Test

---
+++ test/test_distcovery.py
@@ -0,0 +1,11 @@
+import unittest
+
+from distcovery import Test
+
+class TestDistcovery(unittest.TestCase):
+    def test_creation(self):
+        Test()
+
+if __name__ == '__main__':
+    unittest.main()
+
