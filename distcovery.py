8d1af1e331bac27636cd609485eb0180b9bfa9ca
Vasili Vasilyeu <vasili.v@tut.by>
2014-10-24 23:26:19 -0700

    Added method to enumerate test modules

--- distcovery.py
+++ distcovery.py
@@ -1,4 +1,39 @@
+import os
+import re
+
+_TEST_PACKAGE_NAME_REGEX = re.compile('^(test_([a-zA-Z0-9_]+))$')
+_TEST_MODULE_NAME_REGEX = re.compile('^(test_([a-zA-Z0-9_]+))\\.py$')
+
 class Test(object):
     def collect_modules(self):
-        self.test_modules = {}
+        def import_string(sequence):
+            return '.'.join(sequence)
+
+        def sub_item(alias, package, match):
+            return (alias + (match.group(2),), package + (match.group(1),))
+
+        def walk(path, alias, package):
+            if not os.path.isfile(os.path.join(path, '__init__.py')):
+                return
+
+            for name in os.listdir(path):
+                sub_path = os.path.join(path, name)
+
+                match = _TEST_PACKAGE_NAME_REGEX.match(name)
+                if match:
+                    if os.path.isdir(sub_path):
+                        sub_alias, sub_package = sub_item(alias, package, match)
+                        for module in walk(sub_path, sub_alias, sub_package):
+                            yield module
+
+                else:
+                    match = _TEST_MODULE_NAME_REGEX.match(name)
+                    if match and os.path.isfile(sub_path):
+                        yield sub_item(alias, package, match)
+
+        modules = []
+        for alias, module in walk(self.test_root, tuple(), tuple()):
+            modules.append((import_string(alias), import_string(module)))
+
+        self.test_modules = dict(modules)
 
--- test/test_distcovery.py
+++ test/test_distcovery.py
@@ -1,17 +1,111 @@
 import unittest
+import os
+import errno
 
 from distcovery import Test
 
+def _mock_directory_tree(tree):
+    tree = dict([(os.path.join(*key), value) \
+                      for key, value in tree.iteritems()])
+
+    def listdir(path):
+        try:
+            names = tree[path]
+        except KeyError:
+            raise OSError(errno.ENOENT, os.strerror(errno.ENOENT), path)
+
+        if names is None:
+            raise OSError(errno.ENOTDIR, os.strerror(errno.ENOTDIR), path)
+
+        return names
+
+    def isfile(path):
+        try:
+            item = tree[path]
+        except KeyError:
+            return False
+
+        return item is None
+
+    def isdir(path):
+        try:
+            item = tree[path]
+        except KeyError:
+            return False
+
+        return item is not None
+
+    return listdir, isfile, isdir
+
 class TestDistcovery(unittest.TestCase):
+    def setUp(self):
+        super(TestDistcovery, self).setUp()
+
+        self.listdir = os.listdir
+        self.isfile = os.path.isfile
+        self.isdir = os.path.isdir
+
+    def tearDown(self):
+        os.path.isdir = self.isdir
+        os.path.isfile = self.isfile
+        os.listdir = self.listdir
+
+        super(TestDistcovery, self).tearDown()
+
     def test_creation(self):
         test = Test()
         self.assertTrue(isinstance(test, Test))
 
-    def test_collect_modules(self):
+    def test_collect_modules_empty(self):
+        os.listdir, os.path.isfile, os.path.isdir = _mock_directory_tree({})
+
         test = Test()
+        test.test_root = '.'
         test.collect_modules()
         self.assertTrue(hasattr(test, 'test_modules'))
         self.assertEqual(test.test_modules, {})
+
+    def test_collect_modules(self):
+        tree = {('.',): ('__init__.py', 'test_first.py', 'test_second.py',
+                         'test_sub_first', 't_sub_first', 'test_sub_third'),
+                ('.', '__init__.py'): None,
+                ('.', 'test_first.py'): None,
+                ('.', 'test_second.py'): None,
+                ('.', 'test_sub_first'): ('__init__.py', 'test_sub_first.py'),
+                ('.', 'test_sub_first', '__init__.py'): None,
+                ('.', 'test_sub_first', 'test_sub_first.py'): None,
+                ('.', 't_sub_first'): ('__init__.py', 'test_sub_first.py'),
+                ('.', 't_sub_first', '__init__.py'): None,
+                ('.', 't_sub_first', 'test_sub_first.py'): None,
+                ('.', 'test_sub_second'): ('test_sub_first.py',),
+                ('.', 'test_sub_second', 'test_sub_first.py'): None,
+                ('.', 'test_sub_third'): ('__init__.py', 'test_sub_first.py',
+                                          'test_sub_second'),
+                ('.', 'test_sub_third', '__init__.py'): None,
+                ('.', 'test_sub_third', 'test_sub_first.py'): None,
+                ('.', 'test_sub_third', 'test_sub_second'): \
+                    ('__init__.py', 'test_sub_first.py', 't_sub_second.py'),
+                ('.', 'test_sub_third', 'test_sub_second', '__init__.py'): None,
+                ('.', 'test_sub_third', 'test_sub_second',
+                 'test_sub_first.py'): None,
+                ('.', 'test_sub_third', 'test_sub_second',
+                 't_sub_second.py'): None}
+
+        os.listdir, os.path.isfile, os.path.isdir = _mock_directory_tree(tree)
+
+        test = Test()
+        test.test_root = '.'
+        test.collect_modules()
+        self.assertTrue(hasattr(test, 'test_modules'))
+        self.assertEqual(test.test_modules,
+                         {'first': 'test_first',
+                          'second': 'test_second',
+                          'sub_first.sub_first': 'test_sub_first.' \
+                                                 'test_sub_first',
+                          'sub_third.sub_first': 'test_sub_third.' \
+                                                 'test_sub_first',
+                          'sub_third.sub_second.sub_first': \
+                              'test_sub_third.test_sub_second.test_sub_first'})
 
 if __name__ == '__main__':
     unittest.main()
